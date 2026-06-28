9e204acd5731f592a99dc579cb3cb807e45f74fb
mz <mohamedezz22999@gmail.com>
2026-06-28 04:21:47 +0300

    4

--- JWT/auth.js
+++ JWT/auth.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
+let refreshTokens = [];
+
 const app = express();
 app.use(express.json());
 
@@ -17,9 +19,20 @@ const posts= [
     }
 ];
 
+app.post("/token", (req, res) => {
+    const refreshToken = req.body.token;
+    if(refreshToken == null) return res.sendStatus(401);
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403);
+        const accessToken = generateToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    });
+});
+
 app.get("/api",(req, res) => {
     res.json(
-       posts.filter(post => post.username === req.user.name)
+        posts.filter(post => post.username === req.user.name)
     );
 })
 
@@ -30,7 +43,13 @@ app.post("/login", (req, res) => {
     const token = generateToken(user);
 
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
     res.json({ token, refreshToken });
+});
+
+app,delete("/logout", (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
 });
 
 function generateToken(user) {
